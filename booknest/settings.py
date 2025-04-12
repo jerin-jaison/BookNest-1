099899a5793ad3cc36ee3e2f33aafcc1a0ae0610
Jerin Jaison <jerinjaison07@gmail.com>
2025-04-12 09:47:57 +0530

    Update settings.py
    Allowed Host update

--- booknest/settings.py
+++ booknest/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']  
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']  
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
