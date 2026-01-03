e9b19109d098c92e79c17790eb9a5afd4814fdd3
Jerin Jaison <jerinjaison07@gmail.com>
2026-01-03 09:26:26 +0530

    Fix product image rendering using Cloudinary additional_images

--- booknest/urls.py
+++ booknest/urls.py
@@ -1,3 +1,18 @@
+"""
+URL configuration for booknest project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
 
 from django.urls import path, include
@@ -6,8 +21,6 @@ from django.conf.urls.static import static
 from django.conf.urls import handler404
 from user_authentication.views import custom_404_view
 from django.views.static import serve
-from django.contrib import admin
-
 import os
 import re
 
@@ -45,7 +58,6 @@ def case_insensitive_serve(request, path, document_root=None):
 
 urlpatterns = [
     path('', include('user_authentication.urls')),
-    path('admin/', admin.site.urls),
     path('admin_side/', include('admin_side.urls')),
     path('user_profile/', include('user_profile.urls', namespace='user_profile')),
     path('cart_section/', include('cart_section.urls')),
@@ -56,6 +68,25 @@ urlpatterns = [
 
 # Add media files and static files serving
 urlpatterns += [
-    # path('media/<path:path>', case_insensitive_serve, {'document_root': settings.MEDIA_ROOT}),
+    path('media/<path:path>', case_insensitive_serve, {'document_root': settings.MEDIA_ROOT}),
     path('static/<path:path>', case_insensitive_serve, {'document_root': settings.STATIC_ROOT or os.path.join(settings.BASE_DIR, 'static')}),
 ]
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import handler404
+from user_authentication.views import custom_404_view
+
+handler404 = custom_404_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),  
+    path('', include('user_authentication.urls')),
+    path('admin_side/', include('admin_side.urls')),
+    path('user_profile/', include('user_profile.urls', namespace='user_profile')),
+    path('cart_section/', include('cart_section.urls')),
+    path('wallet/', include('user_wallet.urls')),
+    path('payment/', include('online_payment.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+]
+
