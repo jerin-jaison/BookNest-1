46ff03ef14fc08371f6aa076761acade1719d5f7
Jerin Jaison <jerinjaison07@gmail.com>
2026-01-04 08:48:22 +0530

    Chnage of code to get  static images

--- booknest/settings.py
+++ booknest/settings.py
@@ -119,12 +119,12 @@ DATABASES = {
 }
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static'),
-]
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 
