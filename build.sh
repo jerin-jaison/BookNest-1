0517ed9b48af9459bbe6253036d28ca10593f4cd
Jerin Jaison <jerinjaison07@gmail.com>
2026-01-07 20:19:45 +0530

    Fix slug migration in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+# set -o errexit
+
+# pip install -r requirements.txt
+# python manage.py makemigrations
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate --noinput
+python manage.py populate_slugs || echo "Slug population skipped"
 python manage.py collectstatic --noinput
-python manage.py migrate
