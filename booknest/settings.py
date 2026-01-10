9feee3dfcb664adc1e21b4bb0bc656b8b717d111
Jerin Jaison <jerinjaison07@gmail.com>
2026-01-10 17:07:43 +0530

    Add CLOUDINARY config dict to settings.py for SDK support

--- booknest/settings.py
+++ booknest/settings.py
@@ -269,3 +269,9 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
+
+CLOUDINARY = {
+    'cloud_name': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'api_key': os.environ.get('CLOUDINARY_API_KEY'),
+    'api_secret': os.environ.get('CLOUDINARY_API_SECRET'),
+}
